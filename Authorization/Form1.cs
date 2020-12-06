87b32ed3097f2ebe533d65c86e83360169a0e57d
Anastasia Afanasenkova <timosha020702@yandex.ru>
2020-12-07 00:05:11 +0300

    AddDB

---
+++ Authorization/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Authorization
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ProektDataDataContext BD = new ProektDataDataContext();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IQueryable<int> user = BD.User.Where(u => u.Login == textBox1.Text && u.Password == textBox2.Text).Select(u => u.IdRole);
+            if (user.Count() > 0)
+            {
+                MessageBox.Show("Данный пользователь найден");
+            }
+            else
+                MessageBox.Show("Данный пользователь не найден");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form2 reg = new Form2();
+            reg.ShowDialog();
+        }
+    }
+}
---
+++ Authorization/Form2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Authorization
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        ProektDataDataContext BD = new ProektDataDataContext();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text == textBox4.Text)
+            {
+                User NewUser = new User
+                {
+                    Name = textBox1.Text,
+                    Login = textBox2.Text,
+                    Password = textBox3.Text,
+                    IdRole = 2
+                };
+                BD.User.InsertOnSubmit(NewUser);
+                try
+                {
+                    BD.SubmitChanges();
+                    MessageBox.Show("Пользователь зарегистрирован");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+                MessageBox.Show("Пароли не совпадают");
+        }
+    }
+}
